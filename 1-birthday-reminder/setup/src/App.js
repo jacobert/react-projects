7cd712abb36cea917859223715a61b9b6502260c
GIACOMOALBERTAZZI <g.albertazzi@protonmail.ch>
2021-04-16 09:49:59 +0200

    useState Project

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,35 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [list, setList] = useState([...data]);
+
+  const handleList = (list) => {
+    typeof list !== "undefined" && list.length > 0
+      ? setList([])
+      : setList([...data]);
+  };
+
+  return (
+    <React.Fragment>
+      <main>
+        <section className="container">
+          <h3>{list.length} birthdays today</h3>
+          <List people={list} />
+          <button
+            className="btn"
+            type="button"
+            onClick={() => handleList(list)}
+          >
+            {typeof list !== "undefined" && list.length > 0
+              ? "Clear all"
+              : "List all"}
+          </button>
+        </section>
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
