9afc98035494432b92e226b7cb1634701a18da67
GIACOMOALBERTAZZI <g.albertazzi@protonmail.ch>
2021-04-21 20:43:33 +0200

    21-04

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useEffect, useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    let provisionalArray = ["all"];
+
+    items.map((item) => {
+      if (provisionalArray.indexOf(item.category) === -1) {
+        provisionalArray.push(item.category);
+      }
+    });
+
+    setCategories(provisionalArray);
+
+    // Più maniere di filtrare le array con elementi unici
+
+    /* setCategories(
+      provisionalArray.filter((valore, suoIndice, array) => {
+        return array.indexOf(valore) == suoIndice;
+      })
+    ); */
+
+    /*     setCategories([...new Set(provisionalArray)]);     */
+  }, []);
+
   return <h2>menu project setup</h2>;
 }
 
