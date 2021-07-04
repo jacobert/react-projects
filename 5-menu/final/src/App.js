a7ddbbcb61043bc0fbb280b0554123c626def6f7
GIACOMOALBERTAZZI <g.albertazzi@protonmail.ch>
2021-07-04 22:10:57 +0200

    july comeback

--- 5-menu/final/src/App.js
+++ 5-menu/final/src/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
-    if (category === 'all') {
+    if (category === "all") {
       setMenuItems(items);
       return;
     }
--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -1,11 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
-function App() {
-  const [categories, setCategories] = useState([]);
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
+/*
   useEffect(() => {
     let provisionalArray = ["all"];
 
@@ -19,16 +48,11 @@ function App() {
 
     // Più maniere di filtrare le array con elementi unici
 
-    /* setCategories(
+     setCategories(
       provisionalArray.filter((valore, suoIndice, array) => {
         return array.indexOf(valore) == suoIndice;
       })
     ); */
 
-    /*     setCategories([...new Set(provisionalArray)]);     */
-  }, []);
-
-  return <h2>menu project setup</h2>;
-}
-
-export default App;
+/*     setCategories([...new Set(provisionalArray)]);     
+  }, []); */
