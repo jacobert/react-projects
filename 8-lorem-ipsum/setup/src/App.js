0e84b4665f1328262a6a609b6b352ecc58b49b04
GIACOMOALBERTAZZI <g.albertazzi@protonmail.ch>
2021-08-13 15:34:36 +0200

    OK 14-08

--- 8-lorem-ipsum/setup/src/App.js
+++ 8-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,45 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const [disable, setDisable] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+
+    setText(data.slice(0, amount));
+  };
+
+  useEffect(() => {
+    setDisable(!disable);
+  }, [count <= 0 || count > 8]);
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+
+        <button className="btn" type="submit" disabled={disable}>
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
